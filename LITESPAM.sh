64daa6da3ae27fc724da73d67cb1e4ccf0519244
killernoob2 <45244658+killernoob2@users.noreply.github.com>
2018-11-22 04:34:40 +0700

    Create LITESPAM.sh

---
+++ LITESPAM.sh
@@ -0,0 +1,43 @@
+
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+clear
+toilet -f standard -F gay "Email Bomber"
+echo "\033[36;1m"
+python2 6.py
+fi
+
+if
+[ $mrrm = 81 ] || [ $mrrm = 81 ]
+then
+clear
+toilet -f slant --gay "LiteSpam"
+echo "\033[31;1mNama tools: LiteSpam"
+sleep 1
+echo "\033[33;1mKarya: killer_noob (V2 By killer_noon)"
+sleep 1
+echo "\033[32;1mVersi: v2"
+sleep 1
+echo "\033[36;1mTeam: ★Rebuplik of hacker★"
+sleep 1
+echo "\033[34;1mInformasi Lebih Lanjut Kunjungi:??????????????.bitbit"
+echo "\033[31;1m Spesial Thanks To: "
+echo "\033[36;1m"
+echo "XFRIENDS CYBER TEAM"
+echo "まやちゃん"
+echo "Baby Cyber Team"
+echo "SBG-Team"
+echo "Ponorogo Defacer Team"
+sleep 1
+echo "And All Member Republik of hacker"
+echo "\033[30;1m tunggu 8 detik"
+sleep 8
+sh LITESPAM.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Sampai berjumpa

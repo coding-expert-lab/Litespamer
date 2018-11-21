2469f85a6f4553a7309303f828fa9c3adeb94c6e
killernoob2 <45244658+killernoob2@users.noreply.github.com>
2018-11-21 22:30:36 +0000

    Update LITESPAM.sh

--- LITESPAM.sh
+++ LITESPAM.sh
@@ -1,4 +1,70 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "LiteSpam"
+sleep 1
+echo "\033[31;1m Macam-macam tool untuk spam"
+sleep 1
+echo "\033[33;1m Author: killer_noob"
+sleep 1
+echo "\033[32;1m ★contack: +15877711462"
+sleep 1
+echo "\033[34;1m ★Republik of hacker"
+sleep 1
+echo "\033[36;1m Spam Yang Tersedia :"
+echo "1. Tokopedia"
+echo "2. Telkomsel"
+echo "3. Matahari Mall"
+echo "4. PHD"
+echo "5. Jd.Id"
+echo "6. Email Bomber"
+echo "0. Keluar"
+echo "\033[30;1m81. Informasi Tool Ini"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[34;1m"
+figlet "tokopedia"
+php 1.php
+fi
 
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "T-Sel"
+php 2.php
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[31;1m"
+figlet "Mthr Mall"
+php 3.php
+fi
+
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f mono12 -F gay "PHD"
+echo "\033[30;1m"
+php 4.php
+fi
+
+if
+[ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+toilet -f mono12 -F gay "Jd.Id"
+echo "\033[33;1m"
+php 5.php
+fi
+
+if
 [ $mrrm = 6 ] || [ $mrrm = 6 ]
 then
 clear
@@ -40,4 +106,6 @@ if
 then
 echo "\033[31;1m Keluar"
 sleep 1
-echo "\033[32;1m Sampai berjumpa
+echo "\033[32;1m Sampai berjumpa lagi :)"
+sleep 1
+fi

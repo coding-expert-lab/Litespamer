e5687f4758053216424a1c186a281f0351a17f40
killernoob2 <45244658+killernoob2@users.noreply.github.com>
2019-03-21 17:09:28 +0700

    Create 7.php

---
+++ 7.php
@@ -0,0 +1,13 @@
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+toilet -f slant "Bukalapak"
+echo "\033[31;1mJenis Spam:""\033[37;1mChat WhatsApp"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mRepublik of hacker"
+sleep 5
+clear
+cd $HOME/LiTespam
+php 1
+fi
